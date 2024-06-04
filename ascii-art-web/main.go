30bfbc795cea2ed334582e7a976211ba4e52340f
kino <kino@pop-os.localdomain>
2024-06-05 00:55:01 +0200

    ascii web

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -1,70 +1,88 @@
 package main
 
 import (
+	"asciiArt/asciiart"
 	"html/template"
 	"log"
 	"net/http"
-	"asciiArt/asciiart"
 )
 
-
 func main() {
-    http.HandleFunc("/", handler)
-    log.Println("Starting server on :8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatalf("Server failed to start: %v", err)
-    }
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/ascii-art", asciiArtHandler)
+	log.Println("Starting server on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    tmpl, err := template.ParseFiles("templates/index.html")
-    if err != nil {
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        return
-    }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
-    data := struct {
-        Text   string
-        Banner string
-        Art    string
-    }{
-        Text:   "",
-        Banner: "standard",
-        Art:    "",
-    }
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 
-    if r.Method == http.MethodPost {
-        err := r.ParseForm()
-        if err != nil {
-            http.Error(w, "Bad Request", http.StatusBadRequest)
-            return
-        }
+	data := struct {
+		Text   string
+		Banner string
+		Art    string
+	}{
+		Text:   "",
+		Banner: "standard",
+		Art:    "",
+	}
 
-        text := r.FormValue("text")
-        banner := r.FormValue("banner")
+	tmpl.Execute(w, data)
+}
 
-        if text == "" || banner == "" {
-            http.Error(w, "Bad Request", http.StatusBadRequest)
-            return
-        }
+func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.NotFound(w, r)
+		return
+	}
 
-        art, err := asciiart.ASCIIArt(text, banner)
-        if err != nil {
-            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-            return
-        }
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
 
-        data = struct {
-            Text   string
-            Banner string
-            Art    string
-        }{
-            Text:   text,
-            Banner: banner,
-            Art:    art,
-        }
-    }
+	text := r.FormValue("text")
+	banner := r.FormValue("banner")
 
-    tmpl.Execute(w, data)
+	if text == "" || banner == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	art, err := asciiart.ASCIIArt(text, banner)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	data := struct {
+		Text   string
+		Banner string
+		Art    string
+	}{
+		Text:   text,
+		Banner: banner,
+		Art:    art,
+	}
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, data)
 }
