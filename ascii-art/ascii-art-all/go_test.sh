410874803251274441dec15ee11f615842290327
kino <kino@pop-os.localdomain>
2024-06-09 03:44:30 +0200

    testing

--- ascii-art/ascii-art-all/go_test.sh
+++ ascii-art/ascii-art-all/go_test.sh
@@ -1,56 +1,91 @@
 #!/bin/bash
 
-# Define a function that runs multiple commands
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
+# Function to center-align text with color
+text() {
+   local text="$1"
+   local color='\033[0;31m'
+   #local width=$(( ($(tput cols) + ${#text}) / 2))
+   #printf "%*s\n" $width "$(echo -e "${color}${text}${RESET}")"
+   printf "$(echo -e "${color}${text}${RESET}")\n"
+}
+
+# Testing Color and output.
 run_go_commands() {
-    echo "Test 1:"
+    text "Test 1"
     go run . hello | cat -n
     sleep 0.3
 
-    echo "Test 2:"
+    text "Test 2"
     go run . hey
     sleep 0.3
 
-    echo "Test 3:"
+    text "Test 3"
     go run . --color=red ll lllHello
     sleep 0.3
 
-    echo "Test 4:"    
+    text "Test 4"
     go run . "\n\n\n" | cat -n
     sleep 0.3
 
-    echo "Test 5:"
+    text "Test 5"
     go run . "--color=orange" "GuYs" "HeY GuYs?"
     sleep 0.3
 
-    echo "Test 6:"
+    text "Test 6"
     go run . "--color=blue" "B" 'RGB()'
     sleep 0.3
 
-    echo "Test 7:"
+    text "Test 7"
     go run . '--color=yellow' '(%&) ??'
     sleep 0.3
 
-    echo "Test 8:"
+    text "Test 8"
     go run . '--color=green' '1 + 1 = 2'
     sleep 0.3
 
-    echo "Test 9:"
+    text "Test 9"
     go run . --color=blue shadow shadow shadow
     sleep 0.3
 
-    echo "Test 10:"
+    text "Test 10"
     go run . '--color=red' 'hello world'
     sleep 0.3
 
-    echo "Test 11:"
+    text "Test 11"
     go run . '--color=blue' hey --output=h.txt "hey Hello"
     cat h.txt
     sleep 0.3
 
-    echo "Test 12:"
+    text "Test 12"
+    go run . '--color=lemon' "Hello\nWorld"
+
+    text "Test 13"
     go run . '--color=lemon' "Hello\nWorld"
 }
 
+test_reverse() {
+    text "Test 1"
+    go run . --output=test1.txt "       hey     hello  "
+    go run . --reverse=test1.txt | cat -e
+    sleep 0.3
 
-# Define an alias that calls the function
+    text "Test 2"
+    go run . --output=test2.txt '!"#$%&'\''()\n*+,-./012345\n6789:;<=>?@AB\nCDEFGHIJK\nLMNOPQRSTUVW\nXYZ[\]^_`abc\ndefghijk\nlmnopqrst\nuvwxyz{|}~'
+    go run . --reverse=test2.txt
+    sleep 0.3
+
+    text "Test 3"
+    go run . --output=test2.txt " hey \n   What? " 
+    go run . --reverse=test2.txt | cat -e
+    sleep 0.3
+}
+
+# Define an alias that calls the functions
 alias rgc='run_go_commands'
+alias reverse='test_reverse'
