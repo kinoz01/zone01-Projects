a0c4b0d39a6eae1a46e266730d8d6d5db5d56107
kino <kino@pop-os.localdomain>
2024-10-04 01:34:37 +0200

    bim

--- zone01-piscine-js/3. FIND/ion-out.js
+++ zone01-piscine-js/3. FIND/ion-out.js
@@ -1,5 +1,9 @@
 function ionOut(str) {
+    return str.match(/(\S+t)(?=ion)/gi) || []
+}
 
+// golang do not support lookahead assertions:
+function inOUtlong(str) {
     let r =[]
     const regex = /(\S+t)(ion)/g
     const matches = [...str.matchAll(regex)]
