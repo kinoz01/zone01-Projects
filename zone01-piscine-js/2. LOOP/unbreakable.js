97e4fe0838b988452899f8aa4a85624f77f0edbe
kino <kino@pop-os.localdomain>
2024-10-03 02:52:38 +0200

    js

--- zone01-piscine-js/2. LOOP/unbreakable.js
+++ zone01-piscine-js/2. LOOP/unbreakable.js
@@ -1,6 +1,46 @@
-function split(str, sep){
+function split(str, sep) {
+    let index = 0
+    let res = []
 
+    if (sep === '') {
+        for (let i = 0; i < str.length; i++) {
+            res.push(str[i]);
+        }
+        return res;
+    }
+
+    for (let i = 0; i <= str.length - sep.length;) {
+        if (str.slice(i, i + sep.length) === sep) {
+            res.push(str.slice(index, i))
+            index = i + sep.length
+            i = index
+        } else {
+            i++
+        }
+    }
+    res.push(str.slice(index))
+    return res
+}
+
+function join(arr, char) {
+    let str = ""
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i]
+        if (i !== arr.length - 1) {
+            str += char
+        }
+    }
+    return str
 }
 
 const str = "hey    ho w are you  k  "
-console.log(str.split(" "));
+console.log(split(str, " "));
+
+console.log(split('a b c', ' '))
+console.log(split('ggg - ddd - b', ' - '))
+console.log(split('ee,ff,g,', ','));
+console.log(split('Riad', ' '));
+console.log(split('rrrr', 'rr'));
+console.log(split('rrirr', 'rr'));
+console.log(split('Riad', ''));
+console.log(split('', 'Riad'));
