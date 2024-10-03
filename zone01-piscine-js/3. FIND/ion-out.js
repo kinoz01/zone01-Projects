49928c75c37abb7cd023ddb9a2283c6855e61918
kino <kino@pop-os.localdomain>
2024-10-04 01:29:11 +0200

    bim

---
+++ zone01-piscine-js/3. FIND/ion-out.js
@@ -0,0 +1,13 @@
+function ionOut(str) {
+
+    let r =[]
+    const regex = /(\S+t)(ion)/g
+    const matches = [...str.matchAll(regex)]
+
+    for (let match of matches) {
+        r.push(match[1])
+    }
+    return r
+}
+
+console.log(ionOut('1st position is the vision of the 2nd position'));
