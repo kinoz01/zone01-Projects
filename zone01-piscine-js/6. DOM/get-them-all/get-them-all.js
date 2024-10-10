9daa8bdcf809e36816e4148f750919626e65893b
kino <kino@pop-os.localdomain>
2024-10-10 16:38:17 +0200

    hey

---
+++ zone01-piscine-js/6. DOM/get-them-all/get-them-all.js
@@ -0,0 +1,28 @@
+export const getArchitects = () => {
+    let architects = Array.from(document.body.getElementsByTagName('a'))
+    let nonArchitects = Array.from(document.body.getElementsByTagName('a')).filter(elem => elem.tagName !== 'a')
+    console.log(nonArchitects);
+    return [architects, nonArchitects]
+}
+
+export const getClassical = () => {
+    let architects = getArchitects()[0]
+    let classical = architects.filter(elm => elm.classList.contains('classical'))
+    let others = architects.filter(elm => !classical.includes(elm))
+
+    return [classical, others]
+}
+
+export const getActive = () => {
+    let classical = getClassical()[0]
+    let active = classical.filter(elem => elem.classList.contains('active'))
+    let others = classical.filter(elem => !active.includes(elem))
+
+    return [active, others]
+}
+
+export const getBonannoPisano = () => {
+    return [document.getElementById('BonannoPisano'), getActive()[0]]
+}
+
+getArchitects()
