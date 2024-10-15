caceacb6cb26e7afe1124189f8a7140ad245aac8
kino <kino@pop-os.localdomain>
2024-10-15 20:15:32 +0200

    js

--- zone01-piscine-js/7. OBJECT/neuron.js
+++ zone01-piscine-js/7. OBJECT/neuron.js
@@ -1,3 +1,25 @@
-const neuron = (arr) => {
-    
+const neuron = (strArr) => {
+    let result = {}
+
+    strArr.forEach(str => {
+        let [words, response] = str.split('/Response:/')
+        words = words.split(/[:-]/)
+        let type = words[0].toLowercase()
+        if (!res[type]) res[type] = {}
+        const elm = words[1].trim().split(' ').join('_').split(/\W/).join('').toLowerCase()
+        if (!res[type][elm]) {
+            res[type][elm] = {}
+            res[type][elm][type.slice(0, -1)] = words[1].trim()
+            res[type][elm]['responses'] = []
+        }
+        res[type][elm]['responses'].push(response.trim())
+    });
 }
+
+console.log(neuron([
+    'Questions: what is ounces? - Response: Ounce, unit of weight in the avoirdupois system',
+    'Questions: what is ounces? - Response: equal to 1/16 pound (437 1/2 grains)',
+    'Questions: what is Mud dauber - Response: Mud dauber is a name commonly applied to a number of wasps',
+    'Orders: shutdown! - Response: Yes Sr!',
+    'Orders: Quote something! - Response: Pursue what catches your heart, not what catches your eyes.'
+  ]));
