8210a6ffc3101ec522e16a0cf09b8e41b2014136
kino <kino@pop-os.localdomain>
2024-10-15 20:25:36 +0200

    js

--- zone01-piscine-js/7. OBJECT/neuron.js
+++ zone01-piscine-js/7. OBJECT/neuron.js
@@ -1,10 +1,10 @@
 const neuron = (strArr) => {
-    let result = {}
+    let res = {}
 
     strArr.forEach(str => {
-        let [words, response] = str.split('/Response:/')
+        let [words, response] = str.split(/Response:/)
         words = words.split(/[:-]/)
-        let type = words[0].toLowercase()
+        let type = words[0].toLowerCase()
         if (!res[type]) res[type] = {}
         const elm = words[1].trim().split(' ').join('_').split(/\W/).join('').toLowerCase()
         if (!res[type][elm]) {
@@ -12,14 +12,15 @@ const neuron = (strArr) => {
             res[type][elm][type.slice(0, -1)] = words[1].trim()
             res[type][elm]['responses'] = []
         }
-        res[type][elm]['responses'].push(response.trim())
+        res[type][elm]['responses'].push(response)
     });
+    return res
 }
 
-console.log(neuron([
+console.dir(neuron([
     'Questions: what is ounces? - Response: Ounce, unit of weight in the avoirdupois system',
     'Questions: what is ounces? - Response: equal to 1/16 pound (437 1/2 grains)',
     'Questions: what is Mud dauber - Response: Mud dauber is a name commonly applied to a number of wasps',
     'Orders: shutdown! - Response: Yes Sr!',
     'Orders: Quote something! - Response: Pursue what catches your heart, not what catches your eyes.'
-  ]));
+]), { depth: null });
